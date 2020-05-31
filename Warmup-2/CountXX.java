eb1cbce0d089f38631df788dc3851acc8d009cb4
MoshiurShuvo10 <shuvorm87@gmail.com>
2020-05-31 16:33:23 +0600

    Warmup-2

---
+++ Warmup-2/CountXX.java
@@ -0,0 +1,17 @@
+
+Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+
+countXX("abcxx") → 1
+countXX("xxx") → 2
+countXX("xxxx") → 3
+
+int countXX(String str) {
+  int totalXX = 0 ; 
+  for(int i = 0 ; i < str.length()-1 ; i++){
+    if(str.substring(i,i+2).contains("xx"))
+      totalXX++ ;
+  }
+  return totalXX;
+}
+
